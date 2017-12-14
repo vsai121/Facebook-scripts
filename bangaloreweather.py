039f1f36f3fdb8842c39a166dc57521ee3d2484a
V.Saicharan <vsaicharan1998@gmail.com>
2017-12-14 17:30:44 +0530

    Almost done
    Python script to collect Bangalore weather for next 15 days

---
+++ bangaloreweather.py
@@ -0,0 +1,103 @@
+from urllib.request import urlopen
+from urllib.error import HTTPError
+from bs4 import BeautifulSoup as bs
+from tabulate import tabulate
+
+
+def getpage(url):
+    try:
+        page = urlopen(url)
+    except HTTPError as e:
+        return None
+    
+    return page
+    
+
+days=[]
+dates=[]
+conditions = []
+temperatures = []
+maxTemps = []
+minTemps=[]
+summary=[]
+
+
+
+page = getpage("https://www.skymetweather.com/forecast/weather/india/karnataka/bangalore%20urban/bengaluru/extended-forecast/")
+html = page.read()
+bsObj = bs(html , "lxml")
+
+#print(bsObj.prettify())
+
+tables = bsObj.find_all(class_ = "Forecast7Day")
+table = tables[0]
+
+forecast15days = table.find_all(class_ = "forecastBox")
+
+for forecast in forecast15days:
+    box = forecast.find(class_="fcBox")
+    
+    heading = box.find(class_ ="forecastHeading")
+    
+    day = heading.string
+    days.append(day)
+    
+    dateBox = box.find(class_ = "fcDate")
+    date = dateBox.string
+    dates.append(date)
+    
+    conditionBox = box.find(class_ = "fcConditionText")
+    condition = conditionBox.string
+    conditions.append(condition)
+    
+    tempBox= box.find(class_ = "fcTemperature")
+    
+    maxTempBox = tempBox.find(class_ = "maxt")
+    cmaxBox = maxTempBox.find(class_ ="c")
+    maxTemps.append(cmaxBox.string)
+    
+    minTempBox = tempBox.find(class_ = "mint")
+    cminBox = minTempBox.find(class_ ="c")
+    minTemps.append(cminBox.string)
+    
+    cond = forecast.find(class_="WeatherCondition")
+    summary.append(cond.string)
+    
+    
+weathers = []
+for i in range(5):
+    weather=[]
+    weather.append(days[i])
+    weather.append(dates[i])
+    weather.append(conditions[i])
+    weather.append(maxTemps[i])
+    weather.append(minTemps[i])
+    weather.append(summary[i])
+    weathers.append(weather)
+        
+print(tabulate( weathers , headers=["DAY" ,"Date" ,"Condition" , "Max temp" , "Min temp" , "Summary"]))    
+     
+    
+    
+    
+    
+    
+    
+    
+    
+    
+   
+    
+    
+    
+    
+    
+    
+    
+    
+
+
+
+
+
+
