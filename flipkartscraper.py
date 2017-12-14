7ee1cf2be6579429f94d3bcb0e04866cbf2e31ca
V.Saicharan <vsaicharan1998@gmail.com>
2017-12-14 22:40:58 +0530

    Basic flipkart laptops scraper
    Python script to scrape flipkart site for laptops

---
+++ flipkartscraper.py
@@ -0,0 +1,119 @@
+import pandas as pd
+from urllib.request import urlopen
+from urllib.error import HTTPError
+from bs4 import BeautifulSoup as bs
+from tabulate import tabulate
+
+desc=[]
+physical=[]
+specs=[]
+prices=[]
+database=[]
+
+mainUrl = "https://www.flipkart.com/laptops/pr?page="
+pageNumber=1
+nextUrl = "&sid=6bo%2Cb5g&viewType=list"
+
+def get_url(mainUrl , pageNumber , nextUrl):
+    return mainUrl + str(pageNumber) + nextUrl
+
+def getpage(url):
+    try:
+        page = urlopen(url)
+    except HTTPError as e:
+        return None
+    
+    return page
+    
+def calc_laptops_page(desc , physical , specs , prices):
+    laptops = []
+    for i in range(len(desc)):
+        laptop=[]
+        laptop.append(desc[i])
+        laptop.append(physical[i])
+        laptop.append(prices[i])
+        laptop.append(specs[i])
+        laptops.append(laptop)  
+        
+    return laptops
+    
+def print_table(laptops):
+        print(tabulate( laptops , headers=["Description" ,"Physical" ,"Prices"])) 
+    
+def extract_data(bsObj):
+    table = bsObj.select("._2-gKeQ")
+
+    for col in table:
+        laptop_col = col.select("._1-2Iqu")[0]
+    
+        laptop_desc_col = laptop_col.select("._3wU53n")[0]
+        laptop_desc = laptop_desc_col.string
+        desc.append(laptop_desc[0:40])
+    
+        laptop_phy_col = laptop_col.select(".OiPjke")[0]
+        laptop_phy = laptop_phy_col.string
+        physical.append(laptop_phy)
+    
+        laptop_specs_cols=laptop_col.select(".tVe95H")
+        laptop_specs=[]
+        for laptop_specs_col in laptop_specs_cols:
+            laptop_specs.append(laptop_specs_col.string)
+            specs.append(laptop_specs)
+        
+        
+        price_col = laptop_col.select("._2rQ-NK")[0]
+        laptop_price = price_col.get_text()
+        prices.append(laptop_price)  
+        
+def extract_data_multiple_pages(pages):
+    for i in range(pages):
+        #print("PAGE" + str(i+1))
+        #print("_______________________________________________________________________________________________________________________________")
+        
+        url = get_url(mainUrl , i+1 , nextUrl)
+        page = getpage(url)
+
+        if page==None:
+            print("Error reading page")
+    
+        html = page.read()
+        bsObj = bs(html , "lxml")
+
+        extract_data(bsObj)
+
+#print(bsObj.prettify())
+
+
+        laptops = calc_laptops_page(desc , physical , specs , prices)
+        #print_table(laptops)
+        database.append(laptops)
+        
+       
+if __name__ == "__main__":
+    extract_data_multiple_pages(5)
+    print(database[0][1])
+    
+    
+    
+                    
+    
+    
+
+
+
+
+    
+    
+     
+        
+        
+    
+    
+    
+    
+    
+    
+    
+
+
+
